482ae92775e10a5a295066397d3b2bc3c37d729f
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2026-01-30 21:41:26 +0530

    Add Terraform variables for AWS region and application name

---
+++ Serverless-Image-Processing-UsingAWS/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "app_name" {
+  description = "Application name prefix"
+  type        = string
+  default     = "image-pipeline"
+}
